4732f47d036e3cd7d31aa6cab0ff0d01e8b09ee7
re-indweller <asf.ayrat@gmail.com>
2024-10-07 19:29:47 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,87 @@
+# Домашнее задание по теме "Зачем нужно наследование"
+# Задача "Съедобное, несъедобное":
+
+# class Animal:
+#     alive = True # (живой)
+#     fed = False # (накормленный)
+#     def __init__(self, name):
+#         self.name = name
+#
+# class Plant:
+#     edible = False # (съедобность)
+#     def __init__(self, name):
+#         self.name = name
+#
+# class Mammal(Animal):
+#     def eat (self, food):
+#         if food.edible:
+#             print(f"{self.name} съел {food.name}")
+#             self.fed = True
+#         else:
+#             print(f'{self.name} не стал есть {food.name}')
+#             self.alive = False
+#
+# class Predator(Animal):
+#     def eat(self, food):
+#         if food.edible:
+#             print(f'{self.name} съел {food.name}')
+#             self.fed = True
+#         else:
+#             print(f'{self.name} не стал есть {food.name}')
+#             self.alive = False
+#
+# class Flower(Plant):
+#     pass
+#
+# class Fruit(Plant):
+#     edible = True
+
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.alive = True
+        self.fed = False
+
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+class Plant:
+    def __init__(self, name):
+        self.name = name
+        self.edible = False
+
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
