117639ce2ddabb31623b6efdb6880c51511dcdba
re-indweller <asf.ayrat@gmail.com>
2024-10-08 21:55:56 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -19,14 +19,16 @@ class Eagle:  # класс описывающий орла
 
 
 class Pegasus(Horse, Eagle):  # класс описывающий пегаса
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):  # где dx и dy изменения дистанции.
         # В этом методе должны запускаться наследованные методы run и fly соответственно.
         self.run(dx)
         self.fly(dy)
 
-    def get_pos(
-            self):  # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
+    def get_pos(self):  # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
         return self.x_distance, self.y_distance
 
     def voice(self):  # который печатает значение унаследованного атрибута sound
